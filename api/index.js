022fbe6be01cda543cc881bee39081096bab156b
waldohidalgo <whidalgohp@gmail.com>
2024-06-19 15:33:35 -0400

    se configura cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 import express from "express";
-
+import cors from "cors";
 import router from "./router/routes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+var whitelist = ["https://waldohidalgo.vercel.app/", "http://localhost:3000/"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use("/", router);
 
